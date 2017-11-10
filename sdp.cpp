62790f63db2c3908fffe20f594fa6d1f8f8926dd
oswystan <oswystan@126.com>
2017-11-10 19:02:57 +0800

    feature: add sdp factory and type2str

--- sdp.cpp
+++ sdp.cpp
@@ -9,12 +9,46 @@
  *       Revision:  initial draft;
  **************************************************************************************
  */
+#include <algorithm>
+
 #define LOG_TAG "sdp"
 #include "log.h"
 #include "sdp.h"
 
 namespace sdp {
 
+//==================================================
+//     type 2 str utils
+//==================================================
+#define BUILD_TYPE(s,t) {type:t, str:s}
+struct Type2Str {
+    int type;
+    const char* str;
+};
+Type2Str gnodes[] = {
+    BUILD_TYPE("v", SDP_NODE_VERSION),
+    BUILD_TYPE("o", SDP_NODE_ORIGIN),
+    BUILD_TYPE("s", SDP_NODE_SESSION_NAME),
+    BUILD_TYPE("i", SDP_NODE_SESSION_INFORMATION),
+    BUILD_TYPE("u", SDP_NODE_URI),
+    BUILD_TYPE("e", SDP_NODE_EMAIL),
+    BUILD_TYPE("p", SDP_NODE_PHONE),
+    BUILD_TYPE("c", SDP_NODE_CONNECTION),
+    BUILD_TYPE("b", SDP_NODE_BANDWIDTH),
+    BUILD_TYPE("z", SDP_NODE_TZ),
+    BUILD_TYPE("k", SDP_NODE_ENCRYPTION),
+    BUILD_TYPE("t", SDP_NODE_TIME),
+    BUILD_TYPE("r", SDP_NODE_REPEAT),
+    BUILD_TYPE("m", SDP_NODE_MEDIA),
+    BUILD_TYPE("a", SDP_NODE_ATTRIBUTE)
+};
+Type2Str gattrs[] = {
+    BUILD_TYPE("a=rtcp:", SDP_ATTR_RTCP),
+};
+
+//==================================================
+//     sdp reader and writer
+//==================================================
 int SdpReader::parse(std::string& s, SdpRoot& sdp) {
     return 0;
 }
@@ -23,6 +57,9 @@ int SdpWriter::writeTo(std::string& s, SdpRoot& sdp) {
     return 0;
 }
 
+//==================================================
+//     sdp node
+//==================================================
 SdpNode::~SdpNode() {
     for (unsigned int i = 0; i < children.size(); i++) {
         delete children[i];
@@ -57,34 +94,80 @@ int SdpNode::find(ENodeType t,  SdpNode*& n) {
     }
     return -1;
 }
-int SdpNode::find(EAttrType t,  SdpAttr*& n) {
-    n = NULL;
-    SdpAttr* attr = NULL;
-    find(SDP_NODE_ATTRIBUTE, (SdpNode*&)attr);
-    if (attr && attr->attrType == t) {
-        n = attr;
-        return 0;
+int SdpNode::find(EAttrType t,  SdpAttr*& attr) {
+    attr = NULL;
+    for (unsigned int i = 0; i < children.size(); i++) {
+        if (children[i]->nodeType == SDP_NODE_ATTRIBUTE &&
+            ((SdpAttr*)children[i])->attrType == t) {
+            attr = (SdpAttr*)children[i];
+            return 0;
+        }
     }
     return -1;
 }
-int SdpNode::find(EMediaType t, SdpMedia*& n) {
-    n = NULL;
-    SdpMedia* media = NULL;
-    find(SDP_NODE_MEDIA, (SdpNode*&)media);
-    if (media && media->mediaType == t) {
-        n = media;
-        return 0;
+int SdpNode::find(EMediaType t, SdpMedia*& media) {
+    media = NULL;
+    for (unsigned int i = 0; i < children.size(); i++) {
+        if (children[i]->nodeType == SDP_NODE_MEDIA &&
+            ((SdpMedia*)children[i])->mediaType == t) {
+            media = (SdpMedia*)children[i];
+            return 0;
+        }
     }
+
     return -1;
 }
 int SdpNode::find(ENodeType t,  std::vector<SdpNode*> v) {
+    v.clear();
+    auto matcher = [t,&v](SdpNode* n) {
+        if (n->nodeType == t) {
+            v.push_back(n);
+        }
+    };
+    std::for_each(children.begin(), children.end(), matcher);
     return 0;
 }
-int SdpNode::find(EAttrType t,  std::vector<SdpNode*> v) {
+int SdpNode::find(EAttrType t,  std::vector<SdpAttr*> v) {
+    v.clear();
+    auto matcher = [t,&v](SdpNode* n) {
+        if (n->nodeType == SDP_NODE_ATTRIBUTE) {
+            SdpAttr* attr = (SdpAttr*)n;
+            if (attr->attrType == t) {
+                v.push_back(attr);
+            }
+        }
+    };
+    std::for_each(children.begin(), children.end(), matcher);
     return 0;
 }
 int SdpNode::find(EMediaType t, std::vector<SdpMedia*> v) {
+    v.clear();
+    auto matcher = [t,&v](SdpNode* n) {
+        if (n->nodeType == SDP_NODE_MEDIA) {
+            SdpMedia* media = (SdpMedia*)n;
+            if (media->mediaType == t) {
+                v.push_back(media);
+            }
+        }
+    };
+    std::for_each(children.begin(), children.end(), matcher);
     return 0;
+}
+
+//==================================================
+//     sdp factory
+//==================================================
+ENodeType SdpFactory::getNodeType(std::string& l) {
+    return SDP_NODE_NONE;
+}
+EAttrType SdpFactory::getAttrType(std::string& l) {
+    return SDP_ATTR_NONE;
+}
+SdpNode* SdpFactory::createNode(ENodeType type) {
+    return NULL;
+}
+SdpAttr* SdpFactory::createAttr(EAttrType type) {
+    return NULL;
 }
 
 };
--- sdp.h
+++ sdp.h
@@ -69,26 +69,34 @@ enum EProtoType {
 };
 enum EAttrType {
     SDP_ATTR_NONE = 0,
-    SDP_ATTR_RTCP,
+
+    // rfc4566
+    SDP_ATTR_CAT,
     SDP_ATTR_KEYWDS,
     SDP_ATTR_TOOL,
     SDP_ATTR_PTIME,
-    SDP_ATTR_MAXPTIME,          //5
+    SDP_ATTR_MAXPTIME,
     SDP_ATTR_RTPMAP,
     SDP_ATTR_RECVONLY,
     SDP_ATTR_SENDRECV,
     SDP_ATTR_SENDONLY,
-    SDP_ATTR_INACTIVE,          //10
+    SDP_ATTR_INACTIVE,
     SDP_ATTR_ORIENT,
     SDP_ATTR_TYPE,
     SDP_ATTR_CHARSET,
     SDP_ATTR_SDPLANG,
-    SDP_ATTR_LANG,              //15
+    SDP_ATTR_LANG,
+    SDP_ATTR_FRAMERATE,
+    SDP_ATTR_QUALITY,
+    SDP_ATTR_FMTP,
+
+    //rfc3605
+    SDP_ATTR_RTCP,
     SDP_ATTR_CANDIDATE,
     SDP_ATTR_ICE_UFRAG,
     SDP_ATTR_ICE_PWD,
     SDP_ATTR_FINGERPRINT,
-    SDP_ATTR_MSID_SEMANTIC,     //20
+    SDP_ATTR_MSID_SEMANTIC,
 };
 enum ECandiType {
     SDP_CANDI_NONE,
@@ -132,8 +140,9 @@ public:
     int find(EAttrType t,  SdpAttr*& n);
     int find(EMediaType t, SdpMedia*& n);
     int find(ENodeType t,  std::vector<SdpNode*> v);
-    int find(EAttrType t,  std::vector<SdpNode*> v);
+    int find(EAttrType t,  std::vector<SdpAttr*> v);
     int find(EMediaType t, std::vector<SdpMedia*> v);
+    virtual int parse(std::string& l) { return -1; };
 
 public:
     ENodeType nodeType;
@@ -142,6 +151,14 @@ public:
 class SdpRoot : public SdpNode {
 public:
     SdpRoot() : SdpNode(SDP_NODE_SESSION) {}
+};
+class SdpFactory {
+public:
+    ENodeType getNodeType(std::string& l);
+    EAttrType getAttrType(std::string& l);
+
+    SdpNode* createNode(ENodeType type);
+    SdpAttr* createAttr(EAttrType type);
 };
 
 //======================================
@@ -158,6 +175,7 @@ public:
 class SdpOrigin : public SdpNode {
 public:
     SdpOrigin() : SdpNode(SDP_NODE_ORIGIN) {}
+    int parse(std::string& l);
 public:
     std::string userName;
     std::string sid;
@@ -169,36 +187,42 @@ public:
 class SdpSessName : public SdpNode {
 public:
     SdpSessName() : SdpNode(SDP_NODE_SESSION_NAME) {}
+    int parse(std::string& l);
 public:
     std::string name;
 };
 class SdpSessInfo : public SdpNode {
 public:
     SdpSessInfo() : SdpNode(SDP_NODE_SESSION_INFORMATION) {}
+    int parse(std::string& l);
 public:
     std::string info;
 };
 class SdpUri : public SdpNode {
 public:
     SdpUri() : SdpNode(SDP_NODE_URI) {}
+    int parse(std::string& l);
 public:
     std::string uri;
 };
 class SdpEmail : public SdpNode {
 public:
     SdpEmail() : SdpNode(SDP_NODE_EMAIL) {}
+    int parse(std::string& l);
 public:
     std::string email;
 };
 class SdpPhone : public SdpNode {
 public:
     SdpPhone() : SdpNode(SDP_NODE_PHONE) {}
+    int parse(std::string& l);
 public:
     std::string phone;
 };
 class SdpTime : public SdpNode {
 public:
     SdpTime() : SdpNode(SDP_NODE_TIME) {}
+    int parse(std::string& l);
 public:
     uint64_t start;
     uint64_t stop;
@@ -206,6 +230,7 @@ public:
 class SdpConn : public SdpNode {
 public:
     SdpConn() : SdpNode(SDP_NODE_CONNECTION) {}
+    int parse(std::string& l);
 public:
     ENetType    netType;
     EAddrType   addrType;
@@ -214,6 +239,7 @@ public:
 class SdpMedia : public SdpNode {
 public:
     SdpMedia() : SdpNode(SDP_NODE_CONNECTION) {}
+    int parse(std::string& l);
     int filter(int pt);
     int reject(int pt);
 
@@ -226,6 +252,7 @@ public:
 class SdpAttr : public SdpNode {
 public:
     SdpAttr(EAttrType t) : SdpNode(SDP_NODE_ATTRIBUTE), attrType(t) {}
+    int parse(std::string& l);
 public:
     EAttrType   attrType;
     std::string name;
@@ -234,6 +261,7 @@ public:
 class SdpAttrRTCP : public SdpAttr {
 public:
     SdpAttrRTCP() : SdpAttr(SDP_ATTR_RTCP) {}
+    int parse(std::string& l);
 public:
     uint16_t    port;
     ENetType    netType;
@@ -243,6 +271,7 @@ public:
 class SdpAttrCandi : public SdpAttr {
 public:
     SdpAttrCandi() : SdpAttr(SDP_ATTR_CANDIDATE) {}
+    int parse(std::string& l);
 public:
     std::string foundation;
     uint64_t    compID;
