514609c16bd50d1f07aaa362a1e3624336ec91a2
oswystan <oswystan@126.com>
2018-01-19 21:42:30 +0800

    refactor: change style

--- sdp.cpp
+++ sdp.cpp
@@ -1206,30 +1206,30 @@ static std::string randomUUID() {
 static const uint32_t kCrc32Polynomial = 0xEDB88320;
 static uint32_t kCrc32Table[256] = {0};
 static void EnsureCrc32TableInited() {
-  if (kCrc32Table[ARR_LEN(kCrc32Table) - 1])
-    return;  // already inited
-  for (uint32_t i = 0; i < ARR_LEN(kCrc32Table); ++i) {
-    uint32_t c = i;
-    for (size_t j = 0; j < 8; ++j) {
-      if (c & 1) {
-        c = kCrc32Polynomial ^ (c >> 1);
-      } else {
-        c >>= 1;
-      }
+    if (kCrc32Table[ARR_LEN(kCrc32Table) - 1])
+        return;  // already inited
+    for (uint32_t i = 0; i < ARR_LEN(kCrc32Table); ++i) {
+        uint32_t c = i;
+        for (size_t j = 0; j < 8; ++j) {
+            if (c & 1) {
+                c = kCrc32Polynomial ^ (c >> 1);
+            } else {
+                c >>= 1;
+            }
+        }
+        kCrc32Table[i] = c;
     }
-    kCrc32Table[i] = c;
-  }
 }
 
 uint32_t UpdateCrc32(uint32_t start, const void* buf, size_t len) {
-  EnsureCrc32TableInited();
+    EnsureCrc32TableInited();
 
-  uint32_t c = start ^ 0xFFFFFFFF;
-  const uint8_t* u = static_cast<const uint8_t*>(buf);
-  for (size_t i = 0; i < len; ++i) {
-    c = kCrc32Table[(c ^ u[i]) & 0xFF] ^ (c >> 8);
-  }
-  return c ^ 0xFFFFFFFF;
+    uint32_t c = start ^ 0xFFFFFFFF;
+    const uint8_t* u = static_cast<const uint8_t*>(buf);
+    for (size_t i = 0; i < len; ++i) {
+        c = kCrc32Table[(c ^ u[i]) & 0xFF] ^ (c >> 8);
+    }
+    return c ^ 0xFFFFFFFF;
 }
 
 std::string generateCname() {
